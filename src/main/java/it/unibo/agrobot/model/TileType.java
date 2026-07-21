e76252b0ed664c5eecf873fa37806e2d8c7d264f
yosoycriss <cristian.qorri@studio.unibo.it>
2026-07-21 10:47:15 +0200

    creato TileType.java
    definita tipologia di caselle per griglia di gioco

---
+++ src/main/java/it/unibo/agrobot/model/TileType.java
@@ -0,0 +1,13 @@
+package it.unibo.agrobot.model;
+
+/**
+ * rappresenta la tipologia di casella presente nella griglia di gioco
+ */
+public enum TileType {
+    /** terreno agricolo coltivabile */
+    SOIL,
+    /** pozzo d'acqua per la ricarica del serbatoio del drone */
+    WELL,
+    /** hangar per la ricarica della batteria del drone */
+    HANGAR
+}
