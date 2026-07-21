43632e780d6a46b80805f7bccd5b5946b66dda11
yosoycriss <cristian.qorri@studio.unibo.it>
2026-07-21 17:17:44 +0200

    aggiunti metodi di accesso alla tile e isInBounds per position

--- src/main/java/it/unibo/agrobot/model/Grid.java
+++ src/main/java/it/unibo/agrobot/model/Grid.java
@@ -1,5 +1,7 @@
 package it.unibo.agrobot.model;
 
+import java.util.Optional;
+
 /**
  * rappresenta la griglia logica di gioco, ovvero le zolle
  */
@@ -16,6 +18,23 @@ public interface Grid {
     int getHeight();
 
     /**
+     * restituisce la casella alla coordinata
+     * 
+     * @param x coordinata X
+     * @param y coordinata Y
+     * @return un optional contenente la Tile se le coordinate sono valide, Optional.empty() altrimenti
+     */
+    Optional<Tile> getTile(int x, int y);
+
+    /**
+     * restituisce la casella in corrispondenza di una position
+     * 
+     * @param position la posizione
+     * @return un optional con la casella se all'interno della griglia, Optional.empty() altrimenti
+     */
+    Optional<Tile> getTile(Position position);
+
+    /**
      * verifica se una coordinata (x, y) rientra nei limiti della griglia
      * 
      * @param x coordinata X
@@ -23,4 +42,12 @@ public interface Grid {
      * @return true se nei limiti, false altrimenti
      */
     boolean isInBounds(int x, int y);
+
+    /**
+     * Verifica se una Position rientra nei limiti della griglia di gioco
+     * 
+     * @param position la posizione da verificare
+     * @return true se nei limiti, false altrimenti
+     */
+    boolean isInBounds(Position position);
 }
