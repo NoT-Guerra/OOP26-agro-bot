67e591c05c170b5fde297ba7a26ecfb697e0acf6
yosoycriss <cristian.qorri@studio.unibo.it>
2026-07-22 16:05:52 +0200

    aggiunta interfaccia Crop.java

--- src/main/java/it/unibo/agrobot/model/Crop.java
+++ src/main/java/it/unibo/agrobot/model/Crop.java
@@ -1,2 +1,40 @@
 package it.unibo.agrobot.model;
 
+/**
+ * rappresenta una coltura coltivata
+ */
+public interface Crop {
+
+    /**
+     * @return il nome della coltura
+     */
+    String getName();
+
+    /**
+     * @return lo stato della coltura (SEED, GROWING, MATURE, DEAD)
+     */
+    CropState getState();
+
+    /**
+     * aggiorna i timer interni della pianta (crescita, sete)
+     * da chiamare nel game loop per gestire lo scorrere del tempo
+     * 
+     * @param deltaTime il tempo trascorso dall'ultimo aggiornamento
+     */
+    void update(double deltaTime);
+
+    /**
+     * @return true se la pianta è matura e può essere raccolta, false altrimenti.
+     */
+    boolean isReadyToHarvest();
+
+    /**
+     * @return true se la pianta è morta, false altrimenti.
+     */
+    boolean isDead();
+
+    /**
+     * idrata la pianta 
+     */
+    void water();
+}
