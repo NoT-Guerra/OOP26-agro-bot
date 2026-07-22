7a233cd983564a34782d1c99745d916aef062bf3
yosoycriss <cristian.qorri@studio.unibo.it>
2026-07-22 16:12:26 +0200

    creato AbstractCrop.java
    gestione logica timer per crescita e idratazione pianta

---
+++ src/main/java/it/unibo/agrobot/model/AbstractCrop.java
@@ -0,0 +1,9 @@
+package it.unibo.agrobot.model;
+
+/**
+ * gestisce la logica comune a tutte le colture per i timer di crescita e ifratazione
+ */
+public abstract class AbstractCrop implements Crop {
+    
+
+}
